80c3c88fde9e2b9f355509d73d29663751a6117f
David Celis <me@davidcel.is>
2021-10-21 14:51:00 -0700

    Allow forwarded requests from ngrok in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow forwarded requests from ngrok.
+  config.hosts << /[0-9a-f\-]+\.ngrok\.io/
 end
