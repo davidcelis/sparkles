c979a730844e22a9496c2f5e1ac3979cd933077c
David Celis <me@davidcel.is>
2021-10-25 07:46:20 -0700

    Handle when a channel we haven't stored is unarchived

--- app/lib/slack/events/channel_unarchive.rb
+++ app/lib/slack/events/channel_unarchive.rb
@@ -2,8 +2,25 @@ module Slack
   module Events
     class ChannelUnarchive < Base
       def handle
-        channel = ::Channel.find(payload[:channel])
-        channel.update!(archived: false)
+        # If we already have the channel stored, we only need to update the
+        # flag to reflect its been unarchived
+        if (channel = ::Channel.find_by(id: payload[:channel])
+          channel.update!(archived: false)
+
+          return {ok: true}
+        end
+
+        # If we don't have it stored, it means we were installed after this
+        # channel was created and archived and never fetched it in the first
+        # place. We need to get the full object from the API to store it.
+        response = team.api_client.conversations_info(channel: payload[:channel])
+        slack_channel = Slack::Channel.from_api_response(response.channel)
+
+        ::Channel.upsert(slack_channel.attributes)
+
+        # Also join the channel so that if it is eventually made private,
+        # we won't lose access.
+        team.api_client.conversations_join(channel: slack_channel.id)
 
         {ok: true}
       end
