c9331a787dc86d0d03ffd1585a9e4bbb250585a6
David Celis <me@davidcel.is>
2021-10-26 07:13:15 -0700

    Disable Sentry's performance monitoring feature

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,6 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Capture and report all errors.
-  config.traces_sample_rate = 1.0
+  # Don't use Sentry for performance monitoring; stick to errors.
+  config.traces_sample_rate = 0
 end
