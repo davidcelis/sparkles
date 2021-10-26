b26ebf39168920dfa577f3034b443ff84173d8f9
David Celis <me@davidcel.is>
2021-10-26 10:45:45 -0700

    Use Slack-hosted auth buttons

--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -25,7 +25,9 @@ module SlackHelper
       redirect_uri: slack_openid_callback_url
     }.to_query
 
-    link_to "#{slack_button_svg(size: "24px")} Sign in with Slack".html_safe, authorization_url.to_s, style: sign_in_with_slack_style_tags
+    link_to authorization_url.to_s do
+      image_tag("https://platform.slack-edge.com/img/sign_in_with_slack.png", height: 40, width: 170, srcset: {"https://platform.slack-edge.com/img/sign_in_with_slack.png" => "1x", "https://platform.slack-edge.com/img/sign_in_with_slack@2x.png" => "2x"})
+    end
   end
 
   def add_to_slack_button(state:)
@@ -38,54 +40,8 @@ module SlackHelper
       redirect_uri: slack_oauth_callback_url
     }.to_query
 
-    link_to "#{slack_button_svg(size: "16px")} Add to Slack".html_safe, authorization_url.to_s, style: add_to_slack_style_tags
-  end
-
-  private
-
-  def slack_button_svg(size:)
-    <<~SVG.html_safe
-      <svg xmlns="http://www.w3.org/2000/svg" style="height:#{size};width:#{size};margin-right:12px" viewBox="0 0 122.8 122.8">
-        <path d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a"></path>
-        <path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0"></path>
-        <path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d"></path>
-        <path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z" fill="#ecb22e"></path>
-      </svg>
-    SVG
-  end
-
-  def sign_in_with_slack_style_tags
-    size_tags = [
-      "font-size: 18px",
-      "height: 56px",
-      "width: 296px"
-    ]
-
-    (slack_button_style_tags + size_tags).join(";")
-  end
-
-  def add_to_slack_style_tags
-    size_tags = [
-      "font-size: 14px",
-      "height: 44px",
-      "width: 204px"
-    ]
-
-    (slack_button_style_tags + size_tags).join(";")
-  end
-
-  def slack_button_style_tags
-    [
-      "align-items: center",
-      "color: #000000",
-      "background-color: #ffffff",
-      "border: 1px solid #dddddd",
-      "border-radius: 4px",
-      "display: inline-flex",
-      "font-family: Lato, sans-serif",
-      "font-weight: 600",
-      "justify-content: center",
-      "text-decoration: none"
-    ]
+    link_to authorization_url.to_s do
+      image_tag("https://platform.slack-edge.com/img/add_to_slack.png", height: 40, width: 139, srcset: {"https://platform.slack-edge.com/img/add_to_slack.png" => "1x", "https://platform.slack-edge.com/img/add_to_slack@2x.png" => "2x"})
+    end
   end
 end
