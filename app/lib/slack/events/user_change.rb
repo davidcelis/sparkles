b46cfe1e164bf500dc02930eef926c5f588cde75
David Celis <me@davidcel.is>
2021-11-02 11:31:20 -0700

    Ignore user_change events when the team Slack IDs dont match

--- app/lib/slack/events/user_change.rb
+++ app/lib/slack/events/user_change.rb
@@ -4,7 +4,7 @@ module Slack
       def handle
         # I think we might get these events for users who are associated to a
         # team based on an enterprise grid, so we should just ignore them.
-        return if payload.dig(:user, :team_id).start_with?("E")
+        return if payload.dig(:user, :team_id) != slack_team_id
 
         slack_user = ::Slack::User.from_api_response(payload[:user])
         return if slack_user.bot?
