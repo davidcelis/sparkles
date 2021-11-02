652035244751345e24393482102abff8a0005454
David Celis <me@davidcel.is>
2021-11-02 11:07:54 -0700

    Don't allow bot users to be created by events

--- app/lib/slack/events/user_change.rb
+++ app/lib/slack/events/user_change.rb
@@ -3,6 +3,7 @@ module Slack
     class UserChange < Base
       def handle
         slack_user = ::Slack::User.from_api_response(payload[:user])
+        return if slack_user.bot?
 
         ::User.upsert(slack_user.attributes, unique_by: [:slack_team_id, :slack_id])
       end
