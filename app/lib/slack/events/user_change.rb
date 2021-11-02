3d4cac5daca3937cc96c3c7e0d7b1f172e1a3daa
David Celis <me@davidcel.is>
2021-11-02 11:28:23 -0700

    Ignore user_change events for Enterprise Grid users

--- app/lib/slack/events/user_change.rb
+++ app/lib/slack/events/user_change.rb
@@ -2,6 +2,10 @@ module Slack
   module Events
     class UserChange < Base
       def handle
+        # I think we might get these events for users who are associated to a
+        # team based on an enterprise grid, so we should just ignore them.
+        return if payload.dig(:user, :team_id).start_with?("E")
+
         slack_user = ::Slack::User.from_api_response(payload[:user])
         return if slack_user.bot?
 
