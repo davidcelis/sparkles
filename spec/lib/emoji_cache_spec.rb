95cba24985db01016ff6e7f3dc5ae4c36f66af78
David Celis <me@davidcel.is>
2021-11-24 08:14:42 -0800

    Remove accidentally-committed, incomplete test

--- spec/lib/emoji_cache_spec.rb
+++ spec/lib/emoji_cache_spec.rb
@@ -3,12 +3,6 @@ require "rails_helper"
 RSpec.describe EmojiCache do
   let(:team) { create(:team, :sparkles) }
 
-  describe "#read" do
-    it "hits Slack's Emoji API and stores the results" do
-      expect(Rails.cache.redis).to receive(:write)
-    end
-  end
-
   describe "#bust!" do
     it "clears the team's emoji key" do
       expect(Rails.cache.redis).to receive(:del).with("teams:#{team.slack_id}:emoji")
